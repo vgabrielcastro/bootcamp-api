4f900e66057021a7b798e27f65abe2a0fa2a72f2
Vinicius Castro <vini.gabriell@outlook.com>
2026-03-01 22:18:31 -0300

    chore: update dependencies and add .env for port configuration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-// Import the framework and instantiate it
 import Fastify from "fastify";
 const fastify = Fastify({
   logger: true,
 });
 
-// Declare a route
 fastify.get("/", async function handler() {
   return { hello: "world" };
 });
 
-// Run the server!
 try {
-  await fastify.listen({ port: 4004 });
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
