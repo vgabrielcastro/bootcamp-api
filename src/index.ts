b1558a48ad60d53b159b5beacab2e53fa11d5323
Vinicius Castro <vini.gabriell@outlook.com>
2026-03-01 21:45:14 -0300

    chore: add setting prisma and docker setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!!");
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 4004 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
